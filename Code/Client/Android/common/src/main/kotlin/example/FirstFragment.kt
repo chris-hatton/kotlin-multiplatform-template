df06454f7e47a801770f2ba40f6a11dbe4086206
Chris Hatton <christopherhattonuk@gmail.com>
2019-06-16 23:57:27 +1000

    - Remove unwanted example files from TV target - Register Kotlin src dirs with Android plugin for AS IDE to work properly - Fix unit tests

---
+++ Code/Client/Android/common/src/main/kotlin/example/FirstFragment.kt
@@ -0,0 +1,26 @@
+package example
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import example.common.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+}
