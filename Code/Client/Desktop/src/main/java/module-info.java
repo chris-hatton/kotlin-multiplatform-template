b72c967be188f26829242cafd5b9fa04f580fd1e
Chris Hatton <christopherhattonuk@gmail.com>
2019-07-08 15:06:36 +1000

    - Parking Java 11 efffort here after discovering Ktor limitation:   https://github.com/ktorio/ktor/issues/1137

---
+++ Code/Client/Desktop/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+
+//module exampleApp {
+//
+//    requires javafx.controls;
+//    requires javafx.base;
+//    requires javafx.fxml;
+//    requires javafx.graphics;
+//
+//    requires tornadofx;
+//    requires ktor.client.core.jvm;
+//    requires ktor.client.cio;
+//
+//    exports org.chrishatton.example;
+//}
